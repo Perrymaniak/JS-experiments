94cc638742d568e04d38833dcc5ef9a54183a777
Perrymaniak <perrymaniak@gmail.com>
2018-11-26 22:53:16 +0100

    Second commit!
    Files were moved to GIT folder

---
+++ script.js
@@ -0,0 +1,9 @@
+function displayText(){
+	var x = 'Siemanderko!';
+	alert(x);
+}
+
+function openWindow(){
+	window.open("https://www.youtube.com/watch?v=0wHYHMpHmSg", "myWindow", "width=500,height=300,left=20,top=20,toolbar=No,location=No,scrollbars=no,status=No,resizable=no,fullscreen=No"; "NewWindow.focus(); void(0)";
+	myWindow.moveTo(0,0);
+}
